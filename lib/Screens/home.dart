fe92fe68a391fc7ee67f776baecccbac55e905b3
Syed_Nova <syednova7@gmail.com>
2022-11-11 07:05:30 +0000

    Day 3 Done

---
+++ lib/Screens/home.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Abdul Rehman";
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Container(
+            child: Text("no"),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("XD"),
+        ),
+      ),
+    );
+  }
+}
